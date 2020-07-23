af70716e2ca9460642ba3d0e8187a4b87b8770e3
song <songsm5504@gmail.com>
2020-07-24 01:30:25 +0900

    #2.1 2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,26 +10,30 @@ function stopPainting() {
   painting = false;
 }
 
-function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-function onMouseLeave(event) {
-  stopPainting();
+function onMouseDown(event) {
+  startPainting();
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", onMouseLeave);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
